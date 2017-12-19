1230bddad9dc379bd1b8787c74ede2223de2bd34
Alex Drel <alexdrel@gmail.com>
2017-12-19 23:31:16 +0200

    External trace object

--- index.js
+++ index.js
@@ -1,47 +1,43 @@
 "use strict";
 exports.__esModule = true;
+;
 function traceEventListeners(elem, traceRemoval) {
     var addEventListener = elem.addEventListener, removeEventListener = elem.removeEventListener;
-    if (elem.evr_activeListeners) {
-        return;
-    }
-    elem.evr_activeListeners = [];
-    elem.evr_activeOn = {};
+    var activeListeners = [];
+    var storedOns = {};
     for (var key in elem) {
         if (key.substr(0, 2) == 'on' && elem[key]) {
-            elem.evr_activeOn[key] = elem[key];
+            storedOns[key] = elem[key];
         }
     }
     elem.addEventListener = function (type, listener, options) {
-        elem.evr_activeListeners.push({ type: type, listener: listener, options: options });
+        activeListeners.push({ type: type, listener: listener, options: options });
         addEventListener.call(this, type, listener, options);
     };
     elem.removeEventListener = function (type, listener, options) {
         if (traceRemoval) {
-            for (var index = 0; index != elem.evr_activeListeners.length; index++) {
-                var item = elem.evr_activeListeners[index];
+            for (var index = 0; index != activeListeners.length; index++) {
+                var item = activeListeners[index];
                 // TODO: fix naive options comparision
                 if (type == item.type && listener == item.listener && options == item.options) {
-                    elem.evr_activeListeners.splice(index, 1);
-                    break;
+                    activeListeners.splice(index, 1);
                 }
             }
         }
         removeEventListener.call(this, type, listener, options);
     };
+    var reset = function () {
+        for (var key in elem) {
+            if (key.substr(0, 2) == 'on') {
+                elem[key] = storedOns[key] || null;
+            }
+        }
+        for (var index = 0; index != activeListeners.length; index++) {
+            var _a = activeListeners[index], type = _a.type, listener = _a.listener, options = _a.options;
+            removeEventListener.call(elem, type, listener, options);
+        }
+        activeListeners.splice(0, activeListeners.length);
+    };
+    return { target: elem, activeListeners: activeListeners, storedOns: storedOns, reset: reset };
 }
 exports.traceEventListeners = traceEventListeners;
-function resetEventListeners(elem, traceRemoval) {
-    for (var key in elem) {
-        if (key.substr(0, 2) == 'on') {
-            elem[key] = elem.evr_activeOn[key] || null;
-        }
-    }
-    elem.evr_activeOn = {};
-    for (var index = 0; index != elem.evr_activeListeners.length; index++) {
-        var _a = elem.evr_activeListeners[index], type = _a.type, listener = _a.listener, options = _a.options;
-        removeEventListener.call(elem, type, listener, options);
-    }
-    elem.evr_activeListeners = [];
-}
-exports.resetEventListeners = resetEventListeners;
--- index.ts
+++ index.ts
@@ -4,61 +4,57 @@ interface AddEventListenerArgs {
   options?: boolean | AddEventListenerOptions;
 }
 
-interface onLinsteners {
-  [type: string]: EventListenerOrEventListenerObject;
-}
+export interface TracedTarget {
+  target: EventTarget;
+  activeListeners: AddEventListenerArgs[];
+  storedOns: { [type: string]: EventListenerOrEventListenerObject; };
+  reset: () => void;
+};
 
-type _EventTarget = EventTarget & { evr_activeListeners?: AddEventListenerArgs[]; evr_activeOn?: onLinsteners };
+export function traceEventListeners(elem: EventTarget, traceRemoval?: boolean): TracedTarget {
+  const { addEventListener, removeEventListener } = elem;
 
-export function traceEventListeners(elem: _EventTarget, traceRemoval?: boolean) {
-  let { addEventListener, removeEventListener } = elem;
-
-  if (elem.evr_activeListeners) {
-    return;
-  }
-
-  elem.evr_activeListeners = [];
-  elem.evr_activeOn = {};
+  const activeListeners = [];
+  const storedOns = {};
 
   for (const key in elem) {
     if (key.substr(0, 2) == 'on' && elem[key]) {
-      elem.evr_activeOn[key] = elem[key];
+      storedOns[key] = elem[key];
     }
   }
 
   elem.addEventListener = function(this: EventTarget, type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions) {
-    elem.evr_activeListeners.push({ type, listener, options });
+    activeListeners.push({ type, listener, options });
     addEventListener.call(this, type, listener, options);
   };
 
   elem.removeEventListener = function(this: EventTarget, type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions) {
     if (traceRemoval) {
-      for (let index = 0; index != elem.evr_activeListeners.length; index++) {
-        let item = elem.evr_activeListeners[index];
+      for (let index = 0; index != activeListeners.length; index++) {
+        let item = activeListeners[index];
 
         // TODO: fix naive options comparision
         if (type == item.type && listener == item.listener && options == item.options) {
-          elem.evr_activeListeners.splice(index, 1);
-          break;
+          activeListeners.splice(index, 1);
         }
       }
     }
     removeEventListener.call(this, type, listener, options);
-  }
-}
+  };
 
-export function resetEventListeners(elem: _EventTarget, traceRemoval?: boolean) {
-
-  for (const key in elem) {
-    if (key.substr(0, 2) == 'on') {
-      elem[key] = elem.evr_activeOn[key] || null;
+  const reset = function(this: TracedTarget) {
+    for (const key in elem) {
+      if (key.substr(0, 2) == 'on') {
+        elem[key] = storedOns[key] || null;
+      }
     }
-  }
-  elem.evr_activeOn = {};
 
-  for (let index = 0; index != elem.evr_activeListeners.length; index++) {
-    let { type, listener, options } = elem.evr_activeListeners[index];
-    removeEventListener.call(elem, type, listener, options);
-  }
-  elem.evr_activeListeners = [];
+    for (let index = 0; index != activeListeners.length; index++) {
+      let { type, listener, options } = activeListeners[index];
+      removeEventListener.call(elem, type, listener, options);
+    }
+    activeListeners.splice(0, activeListeners.length);
+  };
+
+  return { target: elem, activeListeners, storedOns, reset };
 }
